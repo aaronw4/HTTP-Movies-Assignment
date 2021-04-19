61429a42e412559c4f1a440e4a2c299d2d4b6611
Aaron Williams <aaronrw4@yahoo.com>
2021-04-18 21:50:31 -0700

    Adjusted handling stars data

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 
 const MovieCard = props => {
   const { title, director, metascore, stars } = props.movie;
+  let starsList = stars.split(',')
   
   return (
     <div className="movie-card">
@@ -15,7 +16,7 @@ const MovieCard = props => {
       </div>
       <h3>Actors</h3>
 
-      {stars.map(star => (
+      {starsList.map(star => (
         <div key={star} className="movie-star">
           {star}
         </div>
