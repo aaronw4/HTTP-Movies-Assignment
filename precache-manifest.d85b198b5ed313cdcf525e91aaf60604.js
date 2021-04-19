4c41bf7efebd37cfb20dc3b53a33c76ea6b343bd
Aaron Williams <aaronrw4@yahoo.com>
2021-04-18 22:12:25 -0700

    Updates

--- precache-manifest.d85b198b5ed313cdcf525e91aaf60604.js
+++ precache-manifest.d85b198b5ed313cdcf525e91aaf60604.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dee3ed2735caa75dd6f8f8bfbc6f4c1c",
+    "revision": "fe07d132684e8f4d18decbd7f5c3ba61",
     "url": "/HTTP-Movies-Assignment/index.html"
   },
   {
-    "revision": "c814fd3fec2bd1184374",
+    "revision": "1a1e0483d3ee4191f906",
     "url": "/HTTP-Movies-Assignment/static/css/main.02c8c0f9.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/HTTP-Movies-Assignment/static/js/2.46c636e1.chunk.js"
   },
   {
-    "revision": "c814fd3fec2bd1184374",
-    "url": "/HTTP-Movies-Assignment/static/js/main.577a44fd.chunk.js"
+    "revision": "1a1e0483d3ee4191f906",
+    "url": "/HTTP-Movies-Assignment/static/js/main.cbb4cbf8.chunk.js"
   },
   {
     "revision": "12f6dc7ec778edf49ce9",
